3b5dae3db794f30418b619425a89f615c8068ace
Vitchew <rudzikwiktor@gmail.com>
2023-08-29 11:56:48 +0200

    Onboarding

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,10 @@
+async function Page() {
+    return (
+        <main className="mx-auto flex max-w-3xl">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-3"></p>
+        </main>
+    )
+}
+
+export default Page
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+module.exports = {
+    generateBuildId: async () => {
+      // You can, for example, get the latest git commit hash here
+      return 'my-build-id'
+    },
+  }
+
 module.exports = nextConfig
